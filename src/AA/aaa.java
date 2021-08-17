a2ea707ea93e0ee2ba861ac048b28890026bcdb8
ADURAN3 <ADURAN6@bancodebogota.com.co>
2021-08-17 14:50:04 -0500

    first commit

---
+++ src/AA/aaa.java
@@ -0,0 +1,30 @@
+package AA;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class aaa {
+	WebDriver driver;
+	
+	public void launchBrowser() {
+		
+System.setProperty("webdriver.gecko.driver", "/Users/aduran6/Documents/PRIMERAUTO/DRIVERS/geckodriver");
+		
+		driver = new FirefoxDriver();
+		driver.get("https://www.amazon.com/-/es/");
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		aaa obj = new aaa ();
+		
+		obj.launchBrowser();
+		
+	}
+
+}
